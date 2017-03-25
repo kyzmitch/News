23d1c7fe28df010b1a9e013d618d5bb7231019ea
Andrey Ermoshin <iermoshin@gmail.com>
2017-03-25 21:12:51 +0300

    Pull to refresh

--- News/News/News/NewsListViewController.swift
+++ News/News/News/NewsListViewController.swift
@@ -12,6 +12,7 @@ class NewsListViewController: BaseViewController {
 
     @IBOutlet weak var newsCollectionView: UICollectionView!
     @IBOutlet weak var loadingView: UIView!
+    let refreshControl = UIRefreshControl()
     private let collectionViewDelegate = NewsCollectionViewDelegate()
     private var newsService: NewsNetworkService!
     
@@ -32,6 +33,57 @@ class NewsListViewController: BaseViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        fetchNews()
+    }
+    
+    public func add(newsService: NewsNetworkService) {
+        self.newsService = newsService
+    }
+    
+    private func setup() {
+        self.newsCollectionView.dataSource = collectionViewDelegate
+        self.newsCollectionView.delegate = collectionViewDelegate
+        self.newsCollectionView.reloadData()
+        
+        self.refreshControl.removeTarget(self, action: nil, for: UIControlEvents.valueChanged)
+        self.refreshControl.addTarget(self, action: #selector(refreshControlAction), for: UIControlEvents.valueChanged)
+        self.newsCollectionView .addSubview(self.refreshControl)
+        self.newsCollectionView.alwaysBounceVertical = true
+    }
+
+    private func updateUi() {
+        self.refreshControl.endRefreshing()
+        
+        switch self.dataMode {
+        case .loading:
+            self.loadingView.alpha = 0
+            self.view.bringSubview(toFront: self.loadingView)
+            UIView.animate(withDuration: 1, animations: { 
+                self.loadingView.alpha = 1
+            })
+            break
+        case .content(let articlesViewModel):
+            self.collectionViewDelegate.model = articlesViewModel
+            self.newsCollectionView.reloadData()
+            
+            let subviewsCount = self.view.subviews.count
+            if subviewsCount > 0 {
+                // Determine if it already on top or not
+                if self.view.subviews[subviewsCount - 1] != self.newsCollectionView {
+                    self.newsCollectionView.alpha = 0
+                    self.view.bringSubview(toFront: self.newsCollectionView)
+                    UIView.animate(withDuration: 1, animations: {
+                        self.newsCollectionView.alpha = 1
+                    })
+                }
+            }
+            
+            
+            break
+        }
+    }
+    
+    private func fetchNews() {
         self.newsService.fetchNews { [weak self] (result) in
             
             switch result {
@@ -49,38 +101,7 @@ class NewsListViewController: BaseViewController {
         }
     }
     
-    public func add(newsService: NewsNetworkService) {
-        self.newsService = newsService
+    @objc private func refreshControlAction() {
+        fetchNews()
     }
-    
-    private func setup() {
-        self.newsCollectionView.dataSource = collectionViewDelegate
-        self.newsCollectionView.delegate = collectionViewDelegate
-        self.newsCollectionView.reloadData()
-    }
-
-    private func updateUi() {
-        switch self.dataMode {
-        case .loading:
-            self.loadingView.alpha = 0
-            self.view.bringSubview(toFront: self.loadingView)
-            UIView.animate(withDuration: 1, animations: { 
-                self.loadingView.alpha = 1
-            })
-            break
-        case .content(let articlesViewModel):
-            self.collectionViewDelegate.model = articlesViewModel
-            self.newsCollectionView.reloadData()
-            
-            self.newsCollectionView.alpha = 0
-            self.view.bringSubview(toFront: self.newsCollectionView)
-            UIView.animate(withDuration: 1, animations: { 
-                self.newsCollectionView.alpha = 1
-            })
-            break
-        }
-    }
-    
-    
-    
 }
